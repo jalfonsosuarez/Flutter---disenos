346c124f9996d4416eb222c420171cbc5732a063
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-24 21:07:55 +0200

    [133] Diseño básico (3)

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -10,48 +10,62 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
 
-          _crearImagen(),
-          _crearTitulo(),
-          _crearAcciones(),
-          _crearTexto(),
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
 
-        ],
+          ],
+        ),
       )
     );
   }
 
   Widget _crearImagen() {
 
-    return Image(
-      image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
     );
 
   }
 
   Widget _crearTitulo() {
 
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
 
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text( 'Lago con puente', style: estiloTitulo,),
-                SizedBox(height: 7.0,),
-                Text( 'Se ve el puente reflejado.', style: estiloSubtitulo,)
-              ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text( 'Lago con puente', style: estiloTitulo,),
+                  SizedBox(height: 7.0,),
+                  Text( 'Se ve el puente reflejado.', style: estiloSubtitulo,)
+                ],
+              ),
             ),
-          ),
 
-          Icon( Icons.star, color: Colors.red, size: 30.0,),
-          Text ( '41', style: TextStyle( fontSize: 20.0 ), )
-        ],
+            Icon( Icons.star, color: Colors.red, size: 30.0,),
+            Text ( '41', style: TextStyle( fontSize: 20.0 ), )
+          ],
+        ),
       ),
     );
 
@@ -86,15 +100,15 @@ class BasicoPage extends StatelessWidget {
 
   Widget _crearTexto() {
 
-
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Sit nostrud deserunt magna esse aliqua commodo anim magna. Nisi commodo mollit ut tempor culpa ipsum nostrud voluptate duis reprehenderit occaecat do. Voluptate ex deserunt anim voluptate dolore est. Officia culpa incididunt excepteur eu minim. Aliquip exercitation sit commodo in enim eu quis sunt ut.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0 ),
+        child: Text(
+          'Sit nostrud deserunt magna esse aliqua commodo anim magna. Nisi commodo mollit ut tempor culpa ipsum nostrud voluptate duis reprehenderit occaecat do. Voluptate ex deserunt anim voluptate dolore est. Officia culpa incididunt excepteur eu minim. Aliquip exercitation sit commodo in enim eu quis sunt ut.',
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
-
 
   }
 
