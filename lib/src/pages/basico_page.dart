c8ed8166b33e522a0b28f72c6db67e5c35be6220
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-24 20:15:45 +0200

    [131] Diseño básico (1)

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -3,12 +3,44 @@ import 'package:flutter/material.dart';
 
 
 class BasicoPage extends StatelessWidget {
+
+  final estiloTitulo    = TextStyle( fontSize: 20.0, fontWeight: FontWeight.bold );
+  final estiloSubtitulo = TextStyle( fontSize: 18.0, color: Colors.grey );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Basico page'),
-      ),
+      body: Column(
+        children: <Widget>[
+
+          Image(
+            image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+          ),
+
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+            child: Row(
+              children: <Widget>[
+
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text( 'Lago con puente', style: estiloTitulo,),
+                      SizedBox(height: 7.0,),
+                      Text( 'Se ve el puente reflejado.', style: estiloSubtitulo,)
+                    ],
+                  ),
+                ),
+
+                Icon( Icons.star, color: Colors.red, size: 30.0,),
+                Text ( '41', style: TextStyle( fontSize: 20.0 ), )
+              ],
+            ),
+          )
+
+        ],
+      )
     );
   }
 }
