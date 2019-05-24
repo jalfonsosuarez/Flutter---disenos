1be17bbae0c898262f2cbe35f00df8d6154cb23c
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-25 00:21:21 +0200

    [140] Diseño compuesto (4)

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -16,6 +16,7 @@ class BotonesPage extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 _titulos(),
+                _botonesRedondeados(),
               ],
             ),
           ),
@@ -123,4 +124,69 @@ class BotonesPage extends StatelessWidget {
 
   }
 
+  Widget _botonesRedondeados() {
+
+    return Table(
+
+      children: [
+        TableRow(
+          children: [
+             _crearBotonRedondeado(),
+             _crearBotonRedondeado(),
+          ]
+        ),
+        TableRow(
+          children: [
+             _crearBotonRedondeado(),
+             _crearBotonRedondeado(),
+          ]
+        ),
+        TableRow(
+          children: [
+             _crearBotonRedondeado(),
+             _crearBotonRedondeado(),
+          ]
+        ),
+        TableRow(
+          children: [
+             _crearBotonRedondeado(),
+             _crearBotonRedondeado(),
+          ]
+        ),
+        TableRow(
+          children: [
+             _crearBotonRedondeado(),
+             _crearBotonRedondeado(),
+          ]
+        ),
+      ],
+
+    );
+  }
+
+  Widget _crearBotonRedondeado() {
+
+    return Container(
+      height: 188.0,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(62, 66, 107, 0.7),
+        borderRadius: BorderRadius.circular(20.0)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SizedBox(height: 5.0),
+          CircleAvatar(
+            backgroundColor: Colors.pinkAccent,
+            radius: 35.0,
+            child: Icon( Icons.swap_calls, color: Colors.white, size: 30.0,),
+          ),
+          Text( 'Algo', style: TextStyle( color: Colors.pinkAccent ), ),
+          SizedBox(height: 5.0),
+        ],
+      ),
+    );
+
+  }
 }
